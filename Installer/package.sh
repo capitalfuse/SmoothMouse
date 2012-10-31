20873fc7c4c5d30c7928d66b5b96ea4ac3236215
Eugene / Dae <dae@cyberic.eu>
2012-10-31 05:06:56 +0400

    Removed a troublesome file existence check in package.sh

--- Installer/package.sh
+++ Installer/package.sh
@@ -13,25 +13,23 @@ then
 fi
 
 # Parameters: file name, internal name, identifier, install location 
-c_pkgbuild () {    
-    if [ -f "Root/$1" ]; then  
-        VERSION=$(defaults read "$(pwd)/Root/$1/Contents/Info" CFBundleVersion)
-        
-        echo "\nBuilding a component package: 
-        file name: $1
-        internal name: $2
-        identifier: $IDENTIFIER.$3
-        install location: $4
-        version: $VERSION\n"
-        
-        pkgbuild \
-            --identifier "$IDENTIFIER.$3" \
-            --component "Root/$1" \
-            --scripts "Scripts/$2/" \
-            --install-location "$4" \
-            --version "$VERSION" \
-            "Components/$2.pkg"
-    fi
+c_pkgbuild () {
+    VERSION=$(defaults read "$(pwd)/Root/$1/Contents/Info" CFBundleVersion)
+    
+    echo "\nBuilding a component package: 
+    file name: $1
+    internal name: $2
+    identifier: $IDENTIFIER.$3
+    install location: $4
+    version: $VERSION\n"
+    
+    pkgbuild \
+        --identifier "$IDENTIFIER.$3" \
+        --component "Root/$1" \
+        --scripts "Scripts/$2/" \
+        --install-location "$4" \
+        --version "$VERSION" \
+        "Components/$2.pkg"
 }
 
 c_pkgbuild "SmoothMouse.kext" "Kext" "SmoothMouseKext" "/System/Library/Extensions/"
