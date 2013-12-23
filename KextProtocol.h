3a7b75f39f92c3857c83e1f6227a3f5f6251d054
Daniel Åkerud <da@CAVE.local>
2013-12-23 16:29:16 +0100

    start of multi client and keyboard support

---
+++ KextProtocol.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef enum {
+    EVENT_TYPE_DEVICE_ADDED,
+    EVENT_TYPE_DEVICE_REMOVED,
+    EVENT_TYPE_POINTING,
+    EVENT_TYPE_KEYBOARD
+} event_type_t;
+
+typedef enum {
+	DEVICE_TYPE_POINTING,
+    DEVICE_TYPE_KEYBOARD,
+    DEVICE_TYPE_UNKNOWN
+} device_type_t;
+
+typedef struct {
+    event_type_t type;
+    uint32_t vendor_id;
+    uint32_t product_id;
+    uint64_t seq;
+    uint64_t timestamp;
+} kext_event_base_t;
+
+typedef struct {
+    kext_event_base_t base;
+} device_added_event_t;
+
+typedef struct {
+    kext_event_base_t base;
+} device_removed_event_t;
+
+typedef struct {
+    kext_event_base_t base;
+	uint32_t buttons;
+	int32_t dx;
+	int32_t dy;
+} pointing_event_t;
+
+typedef struct keyboard_event_s {
+    kext_event_base_t base;
+    unsigned int key;
+} keyboard_event_t;
+
+typedef union {
+    kext_event_base_t base;
+    device_added_event_t device_added;
+    device_removed_event_t device_removed;
+    pointing_event_t pointing;
+    keyboard_event_t keyboard;
+} kext_event_t;
+
+typedef enum {
+    KEXT_METHOD_CONFIGURE_CONNECTION,
+    KEXT_METHOD_CONFIGURE_DEVICE,
+    KEXT_METHOD_NUMBER_OF_METHODS
+} kext_method_t;
+
